4c472bb3ffa2252e87e9a8bab9b25a8a4027d472
Simina Rosioru <simina.rosioru@gmail.com>
2024-01-21 13:47:08 +0200

    Add project files.

---
+++ LibraryApp_ASP.NET_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LibraryApp_ASP.NET_MVC.MvcApplication" Language="C#" %>
---
+++ LibraryApp_ASP.NET_MVC/Models/LibraryContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LibraryApp_ASP.NET_MVC.Models
+{
+    public class LibraryContext:DbContext
+    {
+        //constructor
+        public LibraryContext() : base("LibraryContext") { }
+
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Loan> Loans { get; set; }
+    }
+}
