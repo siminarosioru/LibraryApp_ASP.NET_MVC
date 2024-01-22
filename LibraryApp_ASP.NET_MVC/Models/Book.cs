7219ff46733bfb59124446dd0744115001e91c0c
Simina Rosioru <simina.rosioru@gmail.com>
2024-01-22 20:58:59 +0200

    created the model for Book

--- LibraryApp_ASP.NET_MVC/Models/Book.cs
+++ LibraryApp_ASP.NET_MVC/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace LibraryApp_ASP.NET_MVC.Models
 {
     public class Book
     {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string PublishingHouse { get; set; }
+
+        //constaint
+        [Range(1200,2022), Display(Name="Year of appearance")]
+        public int YearAppearance { get; set; }
     }
 }
