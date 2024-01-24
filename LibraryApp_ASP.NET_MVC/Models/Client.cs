f9ffe17eb6903dee61d552ea5c623bfe2cdbc2a9
Simina Rosioru <simina.rosioru@gmail.com>
2024-01-24 17:25:46 +0200

    created the model for Client

--- LibraryApp_ASP.NET_MVC/Models/Client.cs
+++ LibraryApp_ASP.NET_MVC/Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,16 @@ namespace LibraryApp_ASP.NET_MVC.Models
 {
     public class Client
     {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+
+        //create a validation
+        [MaxLength(13), MinLength(13)]
+        //an error appear when adding diacritical marks or other unnecessary marks
+        [RegularExpression("^[0-9]*$", ErrorMessage ="CNP invalid")]
+        public string CNP { get; set; }
+
+        //connection with Loan
+        public virtual ICollection<Loan> Loans { get; set; }
     }
 }
