a56648d8aa895f1a532c57c242de7ccef457d3fa
Simina Rosioru <simina.rosioru@gmail.com>
2024-01-25 18:39:49 +0200

    created the model for Loan

--- LibraryApp_ASP.NET_MVC/Models/Loan.cs
+++ LibraryApp_ASP.NET_MVC/Models/Loan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,16 @@ namespace LibraryApp_ASP.NET_MVC.Models
 {
     public class Loan
     {
+        public int LoanId { get; set; }
+        public int ClientId { get; set; }
+        public int BookId { get; set; }
+
+        //constraint when miss the date
+        [Display(Name= "Loan date"), DataType(DataType.Date)] 
+        public DateTime LoanDate { get; set; }
+
+        //connection with each class
+        public virtual Client client { get; set; }
+        public virtual Book book { get; set; }
     }
 }
