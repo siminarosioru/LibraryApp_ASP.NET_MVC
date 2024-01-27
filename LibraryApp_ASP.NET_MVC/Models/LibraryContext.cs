1350bd452976c747158520df1309ad0f5331be93
Simina Rosioru <simina.rosioru@gmail.com>
2024-01-27 15:36:45 +0200

    Update LibraryContext.cs
    add the information

--- LibraryApp_ASP.NET_MVC/Models/LibraryContext.cs
+++ LibraryApp_ASP.NET_MVC/Models/LibraryContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -6,11 +6,15 @@ using System.Web;
 
 namespace LibraryApp_ASP.NET_MVC.Models
 {
+    //create the LibraryContext class for the connection
+    //then inherit Dbconetxt, using Entity Framework
     public class LibraryContext:DbContext
     {
+        //here the connection is created, using Entity Framework => then the database will be created after build
         //constructor
         public LibraryContext() : base("LibraryContext") { }
 
+        //datasets for each class
         public virtual DbSet<Client> Clients { get; set; }
         public virtual DbSet<Book> Books { get; set; }
         public virtual DbSet<Loan> Loans { get; set; }
